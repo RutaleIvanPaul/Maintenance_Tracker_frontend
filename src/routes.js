75508d60d0f2a4e03830ef2736ab4cd0bc104431
RutaleIvanPaul <paulivan568@gmail.com>
2018-10-02 17:07:57 +0300

    [Feature #160921213]Administrators should be able to login

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Authentication from "./containers/Authentication/Authentication";
 import CreateRequest from "./containers/Requests/CreateRequest";
 import ViewRequests from "./containers/Requests/ViewRequests";
+import AdminViewRequests from "./containers/Requests/AdminViewRequests";
 import Header from "./containers/Header/Header";
 
 const routes = props => {
@@ -15,6 +16,7 @@ const routes = props => {
           <Route path="/" exact component={Authentication} />
           <Route path="/CreateRequest" exact component={CreateRequest} />
           <Route path="/ViewRequests" exact component={ViewRequests} />
+          <Route path="/AdminViewRequests" exact component={AdminViewRequests} />
         </Switch>
         </div>
       </BrowserRouter>
